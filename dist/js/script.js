b6ed6e8fca628ff918c47ba696032c167f8431f1
ehsan <ehsanmavaei@gmail.com>
2021-02-04 13:26:40 +0330

    complete the sidebar and make it responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,68 @@
 overlay=document.querySelector('.overlay');
+sidebar=document.querySelector('.sidebar');
+content=document.querySelector('.content');
+main=document.querySelector('.main');
 
 document.querySelector('#burger').addEventListener('click',function(){
-    if(this.classList.contains("burgerextend")){
+    
+           if(size()){
+               if(this.classList.contains("burgerextend")){
 
-        console.log('in the if')
-        this.classList.remove("burgerextend");
-        overlay.classList.remove("show");
+               this.classList.remove("burgerextend");
+              overlay.classList.remove('show')
 
-    }else{
-        console.log('in the else')
+                   
+                }else{
+               this.classList.add("burgerextend");
+               overlay.classList.add('show')
+               }
 
-        this.classList.add("burgerextend");
-        overlay.classList.add("show");
+            }
+            else{
 
-    }
+                if(this.classList.contains("burgerextend")){
+
+                    this.classList.remove("burgerextend");
+                   sidebar.classList.remove('minisidebar')
+                   content.classList.remove('padding')
+                   main.classList.remove('padding')
+     
+                        
+                     }else{
+                    this.classList.add("burgerextend");
+                    sidebar.classList.add('minisidebar')
+                    content.classList.add('padding')
+                    main.classList.add('padding')
+
+                    }
+            }
 })
+          
+           
+
+
+ 
+
+
+
+
+
+var size =function (){
+	var value=window.innerWidth;
+      if( value<760){
+          sidebar.classList.add('hide');
+          overlay.classList.remove('hide')
+	return true
+	
+	 
+
+}
+else {
+    sidebar.classList.remove('hide')
+    overlay.classList.add('hide')
+
+return false
+
+
+}
+}
