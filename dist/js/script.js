c4cb21bb054eab2d27fafe530bae03f37a1324a5
ehsan <ehsanmavaei@gmail.com>
2021-02-04 09:43:47 +0330

    make mini sidebar for small screen sizes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,17 @@
+overlay=document.querySelector('.overlay');
+
 document.querySelector('#burger').addEventListener('click',function(){
     if(this.classList.contains("burgerextend")){
+
         console.log('in the if')
         this.classList.remove("burgerextend");
+        overlay.classList.remove("show");
+
     }else{
         console.log('in the else')
 
         this.classList.add("burgerextend");
+        overlay.classList.add("show");
 
     }
 })
